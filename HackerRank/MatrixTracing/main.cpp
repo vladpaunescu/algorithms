e12fd5c7c1c882042101f98ce0b9312b7b9d4b79
Vlad Paunescu <vlad.paunescu@intel.com>
2014-01-21 16:55:49 -0800

    Solved Matrix Tracing - Hacker Rank

--- HackerRank/MatrixTracing/main.cpp
+++ HackerRank/MatrixTracing/main.cpp
@@ -5,62 +5,86 @@
 #include <algorithm>
 
 // #define L 1000
-# define mod 1000000007
+#define P 1000000007
+#define MAX 2000002
 using namespace std;
 
-// int pas[L+1][L+1];
+long fact[MAX];
 
-long long prod(int inf, int sup){
-    long long res = inf;
-    for(int i = inf + 1; i <= sup; i++){
-        res*=i;
-        if (res > mod){
-            res = res % mod;
+void computeFact(){
+    fact[0] = 1;
+    for(int i = 1; i < MAX; ++i){
+        fact[i] = (i * fact[i-1]) % P;
+    }
+}
+// (a * b * c) mod x = ((((a mod x) * (b mod x)) mod x) * (c mod x)) mod x
+long power(int a, int n){
+      
+    if( n == 0){
+        return 1;
+    }
+    if(n % 2 == 0){
+        return  (power(a, n/2) * power(a, n/2)) % P;
+    }
+    return  ((power(a, n/2) * power(a, n/2) % P) * a) % P;
+}
+
+//function modular_pow(base, exponent, modulus)
+//    result := 1
+//    while exponent > 0
+//        if (exponent mod 2 == 1):
+//           result := (result * base) mod modulus
+//        exponent := exponent >> 1
+//        base = (base * base) mod modulus
+//    return result
+
+long modularPow(long base, long exponent, long modulus){
+    long result = 1;
+    while(exponent > 0){
+        if (exponent % 2 == 1){
+            result = (result * base) % modulus;
         }
+        exponent = exponent >> 1;
+        base = (base * base ) % modulus;
     }
-    return res;
+    return result;
 }
 
-long combine(int m, int n){
-    if(m < n){
-        int temp = m;
-        m = n;
-        n = temp;
-    }
-   
-    int i = m + n - 2;
-    int j = n - 1;
+
+
+long  combine(int m, int n) {
+    // solution is (m + n - 2)! / (n - 1)! (m-1)!  mod p
+    //    According to [Fermat’s little theorem’
+    //a^(p-1) mod p = 1
+    //Multiplying both side by a^(-1) we can get inverse of a.
+    //a^(p-1)* a^(-1) mod p = a^(-1) mod p
+    //a^(-1) mod p = a^(p-2) mod p.
+    //As you already know, solution is
+    //((n-1+m-1)! / (n-1)!*(m-1)!) mod p
+    //= (n-1+m-1)! * (n-1)!^(-1) ^ (m-1)!^(-1)         mod p
+    //= (n-1+m-1)! * (n-1)!^(p-2) * (m-1)!^(p-2)       mod p
+    // a*b mod m = ((a mod m)*(b mod m)) mod m
+    long a, b, c;
+    a = fact[n-1 + m-1];
+    b = fact[n-1];
+    c = fact[m-1];
+    b = modularPow(b, P-2, P);
+    c = modularPow(c, P-2, P);
     
-    if (i - j > j){
-        j = i-j;
-    }
-    
-    return (prod(i - j +1, i) / prod(1, j)) % mod;
+    return ((a * b) % P * c) % P;
 }
-
-//void buildPascal(){
-//    for(int i = 1; i <= L; i++){
-//        pas[i][1] = pas[1][i] = 1;
-//    }
-//    for(int i = 2; i <= L; i++){
-//        for(int j = 2; j <= L;j++){
-//            pas[i][j] = (pas[i-1][j] + pas[i][j-1]) % 1000000007;
-//        }
-//    }
-//}
-
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int t;
     int m, n;
-  //  buildPascal();
-    
-    cin >>t;
-    for(int i = 1; i <= t; ++i){
-        cin>>m>>n;
-        long comb = combine(m,n);
-        cout<<comb<<endl;
+    computeFact();
+
+    cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        cin >> m >> n;
+        long comb = combine(m, n);
+        cout << comb << endl;
     }
     return 0;
 }
